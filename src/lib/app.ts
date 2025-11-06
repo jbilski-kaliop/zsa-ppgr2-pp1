a13109c8cf745f5b289fa20d10c1b8d686de22c7
Jakub Bilski <jakub@blsk.pl>
2025-11-06 10:53:01 +0100

    Added path mapping for @ alias.

---
+++ src/lib/app.ts
@@ -0,0 +1,28 @@
+import { getIntValueFromInputElement } from "@/lib/dom/input";
+import { gcd } from "@/lib/math";
+
+export default function runApp(): void {
+  const gcdBtnEl = document.getElementById("calcGcd") as HTMLButtonElement;
+
+  if (gcdBtnEl instanceof HTMLButtonElement) {
+    gcdBtnEl.addEventListener("click", function () {
+      gcdBtnEl.disabled = true;
+
+      setTimeout(() => {
+        const val1: number = getIntValueFromInputElement("value1");
+        const val2: number = getIntValueFromInputElement("value2");
+
+        if (isNaN(val1) || isNaN(val2)) {
+          console.error(`Either value 1: '${val1}' or value 2 '${val2}' has wrong format. Please check input values.`);
+          gcdBtnEl.disabled = false;
+          return;
+        }
+
+        const result = gcd(val1, val2);
+
+        console.log(`GCD(${val1}, ${val2}) = ${result}`);
+        gcdBtnEl.disabled = false;
+      }, 50);
+    });
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,3 @@
-import { getIntValueFromInputElement } from "./lib/dom/input";
-import { gcd } from "./lib/math";
+import runApp from "@/lib/app";
 
-const gcdBtnEl = document.getElementById("calcGcd") as HTMLButtonElement;
-
-if (gcdBtnEl instanceof HTMLButtonElement) {
-  gcdBtnEl.addEventListener("click", function () {
-    gcdBtnEl.disabled = true;
-
-    setTimeout(() => {
-      const val1: number = getIntValueFromInputElement("value1");
-      const val2: number = getIntValueFromInputElement("value2");
-
-      if (isNaN(val1) || isNaN(val2)) {
-        console.error(`Either value 1: '${val1}' or value 2 '${val2}' has wrong format. Please check input values.`);
-        gcdBtnEl.disabled = false;
-        return;
-      }
-
-      const result = gcd(val1, val2);
-
-      console.log(`GCD(${val1}, ${val2}) = ${result}`);
-      gcdBtnEl.disabled = false;
-    }, 50);
-  });
-}
+runApp();
